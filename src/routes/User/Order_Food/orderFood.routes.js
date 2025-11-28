95cce6daa30806cdd8c7550c520c8ee94ddccb96
node1 <node1@flyweis.technology>
2025-11-28 18:14:53 +0530

    avt

--- src/routes/User/Order_Food/orderFood.routes.js
+++ src/routes/User/Order_Food/orderFood.routes.js
@@ -2,12 +2,8 @@ const express = require('express');
 const router = express.Router();
 const { auth } = require('../../../../middleware/auth');
 const { validateParams } = require('../../../../middleware/validation');
-const {
-  getStoreByItemId
-} = require('../../../controllers/OrderFood.controller');
-const {
-  getStoreByItemIdParamsSchema
-} = require('../../../../validators/OrderFood.validator');
+const { getStoreByItemId } = require('../../../controllers/OrderFood.controller');
+const { getStoreByItemIdParamsSchema } = require('../../../../validators/OrderFood.validator');
 
 router.get('/getStoreByItemId/:Item_id', auth, validateParams(getStoreByItemIdParamsSchema), getStoreByItemId);
 
