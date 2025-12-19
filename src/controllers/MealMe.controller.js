4e770cd57151d42886f642153caa47c0ee28869b
node1 <node1@flyweis.technology>
2025-12-19 19:03:23 +0530

    update sets

--- src/controllers/MealMe.controller.js
+++ src/controllers/MealMe.controller.js
@@ -34,7 +34,7 @@ const searchStoresByLocation = asyncHandler(async (req, res) => {
     }
 
     const result = await searchStores(lat, lng);
-console.log("===========================\n\n",result);
+// console.log("===========================\n\n",result);
     if (result.success) {
       return sendSuccess(res, result.data, 'Stores retrieved successfully', 200);
     } else {
